94c8d125630e55dc02912f253bd766eb67adb36c
Aachal-10 <aagarwal@kairosliving.com>
2026-04-03 17:36:31 +0530

    api key

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <p className='text-3xl text-indigo-500'>CoinPulse</p>
-  )
+  return <main className='main-container'>
+    <section className='home-grid'>
+      <div id="coin-overview">
+          <div className='header'>
+            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56}/>
+
+          
+          </div>
+      </div>
+      <p>Trending coins</p>
+    </section>
+
+    <section className='w-full mt-7 space-y-4'>
+      <p>Categories</p>
+
+    </section>
+
+  </main>
 }
 
 export default page
